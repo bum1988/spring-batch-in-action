feb125987a8afb3a4cb08186b313215ebc40b868
yunbeom.noh <yunbeom.noh@sk.com>
2020-02-04 17:59:20 +0900

    4.spring batch Job Flow

--- src/main/java/com/yb/spring/springbatchinaction/job/SimpleJobConfiguration.java
+++ src/main/java/com/yb/spring/springbatchinaction/job/SimpleJobConfiguration.java
@@ -24,15 +24,28 @@ public class SimpleJobConfiguration {
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob") // simpleJob 이란 이름의 Batch Job을 생성
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
     @JobScope
     public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep1") // simpleStep1 이란 이름의 Batch Step을 생성
-                .tasklet((contribution, chunkContext) -> { // Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2") // simpleStep1 이란 이름의 Batch Step을 생성
+                .tasklet((contribution, chunkContext) -> { // Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
+                    log.info(">>>>> This is Step2");
                     log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
